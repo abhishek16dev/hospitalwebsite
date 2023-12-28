249ca0e1e6b2cf3381e839bb0e1b761ffcf20597
abhishek singh <abhishekworkmail9@gmail.com>
2023-12-28 23:07:02 +0530

    footer first

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 
 import Navbar from "./components/Navbar/navbar";
+import Footer from "./components/footer/footer";
+
+
 function App() {
   return (
     <div className="App">
     <h1>Abhishek</h1>
     <Navbar />
+    <Footer />
     </div>
   );
 }
---
+++ src/components/footer/footer.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import './footer.css'
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+      <div>
+
+
+
+      </div>
+
+      <div>
+      <h6>Important Links</h6>
+      <ul>
+        <li> Appointment</li>
+        <li> Doctor </li>
+        <li> Service</li>
+        <li> About us</li>
+      </ul>
+
+
+      </div>
+
+      <div>
+      <h6>Contact Us</h6>
+
+
+      </div>
+      <div>
+      <h6>Newsletter</h6>
+
+      </div>
+
+    </div>
+  )
+}
+
+export default Footer
