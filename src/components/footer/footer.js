3a92ad87839aef1060ec1c40900c71bc94f4a7f0
abhishek singh <abhishekworkmail9@gmail.com>
2023-12-29 00:00:46 +0530

    footer added

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -4,9 +4,13 @@ import './footer.css'
 const Footer = () => {
   return (
     <div className='footer'>
+
+        <div className='footer_parent'>
       <div>
 
-
+      <h6>Medical</h6>
+      <p className='para'>Leading the Way in Medical <br/>
+         Execellence, Trusted Care.</p>
 
       </div>
 
@@ -24,13 +28,29 @@ const Footer = () => {
 
       <div>
       <h6>Contact Us</h6>
+      <ul>
+        <li> Call: (237) 681-812-255</li>
+        <li> Email: fildineesoe@gmail.com </li>
+        <li> Address: 0123 Some place</li>
+        <li> Some country</li>
+      </ul>
 
 
       </div>
       <div>
       <h6>Newsletter</h6>
+      <ul>
+        <li> Appointment</li>
+        <li> Doctor </li>
+        <li> Service</li>
+        <li> About us</li>
+      </ul>
 
       </div>
+      
+      </div>
+
+      <hr className='line'></hr>
 
     </div>
   )
