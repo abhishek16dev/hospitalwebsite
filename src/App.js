fb9705ff67e67c28e71d4c687d02fca3e84f07cf
shyam sunder singh <shyamsunders507@gmail.com>
2023-12-30 00:41:55 +0530

    making homepage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-
 import Navbar from "./components/Navbar/navbar";
+import Home from "./components/home/home";
 
 
 function App() {
   return (
     <div className="App">
     <Navbar />
+    <Home />
     </div>
   );
 }
