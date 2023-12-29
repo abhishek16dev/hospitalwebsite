ee411119a36a886a702aad0c493ab173f7aef957
shyam sunder singh <shyamsunders507@gmail.com>
2023-12-29 18:17:40 +0530

    update

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import './navbar.css'
+import call from '../../assets/call.png'
+
 
 const Navbar = () => {
   return (
     <div>
     <div>
-        <p className='heading'> navbar</p>
+        <p className='heading'> Medical</p>
     </div>
     </div>
   )
